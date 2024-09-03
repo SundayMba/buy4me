5e7778d9a884397cdc88ae3c3bfc0a51178eb1fc
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-09-03 10:02:33 +0100

    Initialize Flask-Migrate and set up migrations

--- api/__init__.py
+++ api/__init__.py
@@ -1,24 +1,25 @@
+# api/__init__.py
+
 from flask import Flask
-from flasgger import Swagger
-from api.v1.routes.main import main as main_blueprint
+from flask_migrate import Migrate
+from api.v1.models.user import db  # Import the db instance
 from api.config import config
+from flasgger import Swagger
 
 swagger = Swagger()
 
 def create_app(env_name):
-    """Flask Application factory function
-    
-    Keyword arguments:
-    argument -- config class
-    Return: Flask App instance
-    """
-    app = Flask(__name__)
-    app.config.from_object(config.get(env_name))
-    swagger.init_app(app)
+	"""Flask Application factory function"""
+	app = Flask(__name__)
+	app.config.from_object(config.get(env_name))
+	swagger.init_app(app)
+	db.init_app(app)
+	migrate = Migrate(app, db)
 
-    """
-        Register Application Blueprint Below
-    """
-    app.register_blueprint(main_blueprint, url_prefix="/api/v1")
+	"""
+	Register Application Blueprint Below
+	"""
+	from api.v1.routes.main import main as main_blueprint
+	app.register_blueprint(main_blueprint, url_prefix="/api/v1")
 
-    return app
+	return app
