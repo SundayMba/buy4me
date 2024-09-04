fefc191a6c99829ac3bab545d808d980aa0e2318
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-09-04 15:14:47 +0100

    https://github.com/SundayMba/buy4me/blob/dev/api/config.py

---
+++ api/cofig.py
@@ -0,0 +1,13 @@
+class Config:
+    SWAGGER = {
+        "title": "BUY4ME E-Commerce Restful API",
+        "uiversion": 3
+    }
+
+
+class Development(Config):
+    DEBUG = True
+
+config = {
+    "dev": Development
+}
