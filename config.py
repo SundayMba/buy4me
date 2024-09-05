f731489e50c99cade9c1636e2740e2c1f20e964f
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-09-05 22:00:55 +0100

    Database Configuration

--- config.py
+++ config.py
@@ -1,25 +1,31 @@
 import os
 
+# Fetch environment variables
 DB_ENGINE=os.getenv("DB_ENGINE")
 DB_USERNAME=os.getenv("DB_USERNAME")
 DB_PASSWORD=os.getenv("DB_PASSWORD")
 DB_HOST=os.getenv("DB_HOST")
-DB_PORT=os.getenv("DB_PORT")
+DB_PORT=os.getenv("DB_PORT", 3306) # Default MySQL port
 DB_NAME=os.getenv("DB_NAME")
 
-
+# Create the database URI
 URI = f"{DB_ENGINE}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 class Config:
-    SWAGGER = {
-        "title": "BUY4ME E-Commerce Restful API",
-        "uiversion": 3
-    }
-    SQLALCHEMY_DATABASE_URI = URI
+	# Swagger configuration
+	SWAGGER = {
+		"title": "BUY4ME E-Commerce Restful API",
+		"uiversion": 3
+	}
+	# SQLAlchemy database configuration
+	SQLALCHEMY_DATABASE_URI = URI
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Development(Config):
-    DEBUG = True
+	# Enable debugging in development
+	DEBUG = True
 
+# Define configuration options
 config = {
-    "dev": Development
+	"dev": Development
 }
