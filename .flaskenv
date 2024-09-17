a65aebd5d38b1f19a5f7b0f9f9248f76525d5064
Akanni23 <muhasy2k@yahoo.com>
2024-09-17 00:30:44 -0700

    update

--- .flaskenv
+++ .flaskenv
@@ -1,7 +1,7 @@
 FLASK_APP=run.py
 FLASK_ENV=dev
 DB_USERNAME="buy4me"
-DB_PORT="5000"
+DB_PORT="3306"
 DB_ENGINE="MySQL"
 DB_PASSWORD="root"
 DB_HOST="localhost"
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Fetch environment variables for database configuration
 DB_ENGINE = os.getenv("DB_ENGINE", "mysql")
-DB_USERNAME = os.getenv("DB_USERNAME", "Matthew")
+DB_USERNAME = os.getenv("DB_USERNAME", "Akanni")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", 3306)
@@ -10,14 +10,14 @@ DB_NAME = os.getenv("DB_NAME", "buy4me")
 
 # Construct the database URI
 # Directly set the database URI
-URI = "mysql://Matthew:root@localhost:3306/buy4me"
+URI = "mysql://Akanni:root@localhost:3306/buy4me"
 
 # Debug the URI (optional)
 print(f"Database URI: {URI}")
 
 class Config:
 	# SQLAlchemy database configuration
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql://Matthew:root@localhost:3306/buy4me')
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql://Akanni:root@localhost:3306/buy4me')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 	# Swagger configuration
